6aab49d38feee74268218c01d7c1a866d9935bc7
Superb Pineapple <221645145+SuperbPineapple@users.noreply.github.com>
2025-12-02 22:17:12 -0600

    uploaded cache clearer, untested
    use this with warning. i have not tested this on an xbox yet

---
+++ XboxCacheClearer.py
@@ -0,0 +1,19 @@
+import os
+
+# define cachepaths, you can change these if your cache path is different
+cachePath = r"E:\CACHE"
+
+# for if you play beta halos (you can comment this out if you don't play it)... probablyu
+betaHaloCache = r"E:\CACHEGSRV"
+
+# remove cache files
+for fileName in os.listdir(cachePath):
+   fileToRemove = os.path.join(cachePath, fileName)
+   if os.path.isfile(fileToRemove):
+      os.remove(fileToRemove)
+
+# remove halo cache files (you can comment this out if you don't play it)
+for fileNameHalo in os.listdir(betaHaloCache):
+   fileToRemoveHalo = os.path.join(betaHaloCache, fileNameHalo)
+   if os.path.isfile(fileToRemoveHalo):
+      os.remove(fileToRemoveHalo)
